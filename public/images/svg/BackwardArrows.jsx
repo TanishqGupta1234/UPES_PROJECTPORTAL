48a3bddaf041ad5300a5bd0699ee32e83c8c409c
TANISHQGUPTA(TAG) <94602846+TanishqGupta1234@users.noreply.github.com>
2024-05-29 10:31:42 +0530

    Add files via upload

---
+++ public/images/svg/BackwardArrows.jsx
@@ -0,0 +1,18 @@
+function BackwardArrows() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      viewBox="0 0 19 19"
+      fill="none"
+    >
+      <g opacity="0.4">
+        <path d="M9.5 14L5 9.5L9.5 5" stroke="#262626" strokeWidth="2" />
+        <path d="M14 14L9.5 9.5L14 5" stroke="#262626" strokeWidth="2" />
+      </g>
+    </svg>
+  );
+}
+
+export default BackwardArrows;
---
+++ public/images/svg/ForwardArrows.jsx
@@ -0,0 +1,16 @@
+function ForwardArrows() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      viewBox="0 0 19 19"
+      fill="none"
+    >
+      <path d="M9.5 14L14 9.5L9.5 5" stroke="#262626" strokeWidth="2" />
+      <path d="M5 14L9.5 9.5L5 5" stroke="#262626" strokeWidth="2" />
+    </svg>
+  );
+}
+
+export default ForwardArrows;
